931e8423232d4284e8d3c5ecf74273ab3bf0393a
Adrien SCHALLER <adrien@pg3.fr>
2020-04-06 08:44:08 +0200

    add issue for docker auth fail

--- cal_setup.py
+++ cal_setup.py
@@ -5,6 +5,8 @@ from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
+# Docker https://github.com/gsuitedevs/python-samples/issues/139
+
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/tasks.readonly']
 CREDENTIALS_FILE = 'credentials.json'
