3cc314d3d0f9ff45c461f46131bbf7438ff91b33
Adrien Schaller <adrien@pg3.fr>
2020-04-10 19:21:52 +0200

    optim code

--- app.py
+++ app.py
@@ -11,12 +11,10 @@ app = Flask(__name__)
 fa = FontAwesome(app)
 
 filesource = "./config.yaml"
-
-# Vars
 sass_map = {"static/scss/style.scss": "static/style.css"}
 events = list()
+labelList = ["webinar","url","tags","city"]
 
-# class
 class HTMLFilter(HTMLParser):
     text = ""
     def handle_data(self, data):
@@ -35,60 +33,59 @@ def open_yaml():
             print(exc)
     return file
 
-# get datas
-def get_datas(idCal,vTags):
+# resplit label after html to text
+def label_split(event,label):
+    event['desc'] = event['desc'].replace(label+":", " "+label+":")
+    return event
+
+# search label to description
+def label_search(event,label,vTags):
+    parse = (searh for searh in event['desc'].split() if re.match(r"^"+label+":.*", searh))
+    for i in parse:
+        if label == "tags":
+            fullTags = i.replace(label+":", "").split(",")
+            event[label] = list( d for d in fullTags if d in vTags )
+        else:
+            event[label] = i.replace(label+":", "").split(",")
+        event['desc'] = event['desc'].replace(i , "")
+    return event
+
+# get event to Google Calendar
+def gcalendar_get(idCal):
     service = get_calendar_service()
     now = datetime.datetime.utcnow().isoformat() + 'Z'
-    # get 100 value
-    events_result = service.events().list(
-        calendarId= idCal, timeMin=now,
-        maxResults=100, singleEvents=True,
-        orderBy='startTime').execute()
+    events_result = service.events().list(calendarId= idCal, timeMin=now,maxResults=100, singleEvents=True,orderBy='startTime').execute()
     events = events_result.get('items', [])
+    return events
+
+# format champs for timeline
+def update_datas(events,vTags):
     for event in events:
-       # update date format start/end
-       start = event['start'].get('dateTime', event['start'].get('date'))
-       event['datestart'] = datetime.datetime.fromisoformat(start).strftime("%d/%m/%y %H:%M")
-       end = event['end'].get('dateTime', event['end'].get('date'))
-       event['dateend'] = datetime.datetime.fromisoformat(end).strftime("%d/%m/%y %H:%M")
+       if 'start' in event:
+           start = event['start'].get('dateTime', event['start'].get('date'))
+           event['datestart'] = datetime.datetime.fromisoformat(start).strftime("%d/%m/%y %H:%M")
+       if 'end' in event:
+           end = event['end'].get('dateTime', event['end'].get('date'))
+           event['dateend'] = datetime.datetime.fromisoformat(end).strftime("%d/%m/%y %H:%M")
        if 'description' in event:
            # description => HTML to text
            desc = HTMLFilter()
            desc.feed(event['description'])
            event['desc'] = desc.text
            # search and clean label
-           # webinar
-           event['desc'] = event['desc'].replace("webinar:", " webinar:")
-           event['desc'] = event['desc'].replace("url:", " url:")
-           event['desc'] = event['desc'].replace("tags:", " tags:")
-           event['desc'] = event['desc'].replace("city:", " city:")
-           webinarparse = (searhwebinar for searhwebinar in event['desc'].split() if re.match(r"^webinar:.*", searhwebinar))
-           for i in webinarparse:
-             event['webinar'] = i.replace("webinar:", "").split(",")
-             event['desc'] = event['desc'].replace(i , "")
-           # url
-           urlparse = (searhurl for searhurl in event['desc'].split() if re.match(r"^url:.*", searhurl))
-           for i in urlparse:
-             event['url'] = i.replace("url:", "").split(",")
-             event['desc'] = event['desc'].replace(i , "")
-           # tags
-           tagsparse = (searhtags for searhtags in event['desc'].split() if re.match(r"^tags:.*", searhtags))
-           for i in tagsparse:
-             fullTags = i.replace("tags:", "").split(",")
-             event['tags'] = list( d for d in fullTags if d in vTags )
-             event['desc'] = event['desc'].replace(i , "")
-           # city
-           cityparse = (searhcity for searhcity in event['desc'].split() if re.match(r"^city:.*", searhcity))
-           for i in cityparse:
-             event['city'] = i.replace("city:", "").split(",")
-             event['desc'] = event['desc'].replace(i , "")
+           for i in labelList:
+             event = label_split(event,i)
+           for i in labelList:
+             event = label_search(event,i,vTags)
     return events
 
+# route
 @app.route('/')
 def accueil():
     compile_sass_to_css(sass_map)
     file = open_yaml()
-    events = get_datas(file['idCalendar'],file['valideTags'])
+    events = gcalendar_get(file['idCalendar'])
+    events = update_datas(events,file['valideTags'])
     return render_template('index.html', events=events, file=file)
 
 @app.errorhandler(404)
@@ -99,8 +96,8 @@ def page_not_found(e):
 
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/png')
+    return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico', mimetype='image/png')
+
 @app.route('/ping')
 def ping():
     return "pong"
